f16af974c8db06dc233d7962dad629ebddc7f9bf
taiming <taiming@bridgewell.com>
2020-06-08 06:05:12 +0800

    Updates

---
+++ precache-manifest.444570e1f7d061fa77b48e565a9f46c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff178ff5c914bbb2d02c45d184efc581",
+    "url": "/good-travel/index.html"
+  },
+  {
+    "revision": "39d0d64185ff5b7694f9",
+    "url": "/good-travel/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0b9448cb29af2dab17aa",
+    "url": "/good-travel/static/js/2.84c166a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/good-travel/static/js/2.84c166a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39d0d64185ff5b7694f9",
+    "url": "/good-travel/static/js/main.f9eebfee.chunk.js"
+  },
+  {
+    "revision": "1cba9244a41bce4b90e4",
+    "url": "/good-travel/static/js/runtime-main.98e1c3fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/good-travel/static/media/logo.5d5d9eef.svg"
+  }
+]);
